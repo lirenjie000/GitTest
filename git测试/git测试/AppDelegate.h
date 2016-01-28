7e6edeae1d2dace4cd27338e2c3ee1ae2741d7a3
lirenjie000 <565803433@qq.com>
2016-01-28 18:03:16 +0800

    1111111

---
+++ git测试/git测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git测试
+//
+//  Created by lirenjie on 16/1/28.
+//  Copyright © 2016年 李人杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git测试/git测试/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  git测试
+//
+//  Created by lirenjie on 16/1/28.
+//  Copyright © 2016年 李人杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"1111");
+    NSLog(@"1111");
+    NSLog(@"1111");
+    NSLog(@"1111");
+    NSLog(@"1111");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
